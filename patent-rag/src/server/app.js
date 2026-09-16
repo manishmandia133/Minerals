05455c6ed079d402e37210efbe1a0c165abbdad7
Devyansh-Dingolia <dingoliadevyansh@gmail.com>
2026-09-16 14:33:05 +0530

    Increase JSON body size limit to 5mb

--- patent-rag/src/server/app.js
+++ patent-rag/src/server/app.js
@@ -5,7 +5,7 @@ import ingestionRouter from "./routes/ingestion.routes.js";
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.use("/api/v1/query", queryRouter);
 app.use("/api/v1/ingestion", ingestionRouter);
